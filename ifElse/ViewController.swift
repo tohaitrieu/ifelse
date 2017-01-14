c65bd516a6b46d9cfd4649739f6c51e1039e501a
Trieu To <tohaitrieu@Trieu-To.local>
2017-01-14 14:50:34 +0700

    Update

--- ifElse/ViewController.swift
+++ ifElse/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtDiem: UITextField!
+    @IBOutlet weak var lblKetQua: UILabel!
+    @IBAction func btnCheck(_ sender: Any) {
+        var toan = Int(txtDiem.text!)!
+            if toan >= 5 {
+                lblKetQua.text = "Dau roi"
+            }
+            else {
+                lblKetQua.text = "Rot roi"
+            }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
